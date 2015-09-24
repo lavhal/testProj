afd2742701ce76bb04f79937c6054de652440962
vagrant <vagrant@precise32.(none)>
2015-09-24 12:48:32 +0000

    configuration of repository

---
+++ VoColClient/Hooks/InstallHooks.sh
@@ -0,0 +1,19 @@
+#Copy implemented hooks 
+for file in `ls -1 VoColClient/Hooks`
+do
+ if [ $file != "InstallHooks.sh" ]; then
+   cp VoColClient/Hooks/$file .git/hooks/$file
+   echo "Please write your admin password to allow execute permission for hook "$file
+   sudo chmod +x .git/hooks/$file
+ fi
+done
+
+#Ignore tools folder using .gitignore
+if [ ! -e ".gitignore" ]; then
+  touch .gitignore
+  echo "VoColClient/" >> .gitignore
+else
+  if ! grep -q "VoColClient" ".gitignore"; then
+   echo "VoColClient/" >> .gitignore
+  fi
+fi
